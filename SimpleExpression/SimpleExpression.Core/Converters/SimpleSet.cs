2eb34a22241e35ae631cddb69c042f5dec6d2e86
Tim Bourguignon <timothepdotnet@gmail.com>
2014-01-17 13:11:38 +0100

    Refactored SimpleSet

--- SimpleExpression/SimpleExpression.Core/Converters/SimpleSet.cs
+++ SimpleExpression/SimpleExpression.Core/Converters/SimpleSet.cs
@@ -1,40 +1,74 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SimpleExpressions.Core.Converters
 {
     internal class SimpleSet: IConverter
     {
+        private const string AndWhitespaces = "AndWhitespaces";
         private readonly IList<string> functions = new List<string> { "Alphanumeric", "Alphanumerics", "Letter", "Letters", "Number", "Numbers" };
 
         public bool CanParse(string token)
         {
-            return this.functions.Contains(token);
+            return this.functions.Any(function => token.Contains(function));
+        }
+
+        private string FormatPattern(bool lower, bool upper, bool number, bool white, bool multiple)
+        {
+            const string lowerCaseRange = "a-z";
+            const string upperCaseRange = "A-Z";
+            const string numberRange = "0-9";
+            const string whitespaces = @"\s";
+            const string multiplicity = "*";
+
+            return string.Format(@"[{0}{1}{2}{3}]{4}",
+                lower ? lowerCaseRange : "", 
+                upper ? upperCaseRange : "", 
+                number ? numberRange : "", 
+                white ? whitespaces : "",
+                multiple ? multiplicity : "");
         }
 
         public IList<string> Generate(IList<Function> tokens, int currentIndex, IList<string> pattern)
         {
-            switch (tokens[currentIndex].Name)
-            {
-                case "Alphanumeric":
-                    pattern.Add(@"[a-zA-Z0-9]");
-                    break;
-                case "Alphanumerics":
-                    pattern.Add(@"[a-zA-Z0-9]*");
-                    break;
-                case "Letter":
-                    pattern.Add(@"[a-zA-Z]");
-                    break;
-                case "Letters":
-                    pattern.Add(@"[a-zA-Z]*");
-                    break;
-                case "Number":
-                    pattern.Add(@"[0-9]");
-                    break;
-                case "Numbers":
-                    pattern.Add(@"[0-9]*");
-                    break;
-            }
+            var formattedPattern = "";
 
+            const bool lower = true;
+            const bool upper = true;
+            const bool number =true;
+            const bool white = true;
+            const bool multiple = true;
+
+            var name = tokens[currentIndex].Name;
+            if (name == "Alphanumerics")
+                formattedPattern = FormatPattern(lower, upper, number, !white, multiple);
+            else if(name.StartsWith("Alphanumerics") && name.EndsWith(AndWhitespaces))
+                formattedPattern = FormatPattern(lower, upper, number, white, multiple);
+            if (name == "Alphanumeric")
+                formattedPattern = FormatPattern(lower, upper, number, !white, !multiple);
+            else if(name.StartsWith("Alphanumeric") && name.EndsWith(AndWhitespaces))
+                formattedPattern = FormatPattern(lower, upper, number, white, !multiple);
+
+            if (name == "Letters")
+                formattedPattern = FormatPattern(lower, upper, !number, !white, multiple);
+            else if (name.StartsWith("Letters") && name.EndsWith(AndWhitespaces))
+                formattedPattern = FormatPattern(lower, upper, !number, white, multiple); 
+            else if (name == "Letter")
+                formattedPattern = FormatPattern(lower, upper, !number, !white, !multiple);
+            else if (name.StartsWith("Letter") && name.EndsWith(AndWhitespaces))
+                formattedPattern = FormatPattern(lower, upper, !number, white, !multiple);
+
+            if (name == "Numbers")
+                formattedPattern = FormatPattern(!lower, !upper, number, !white, multiple);
+            else if (name.StartsWith("Numbers") && name.EndsWith(AndWhitespaces))
+                formattedPattern = FormatPattern(!lower, !upper, number, white, multiple);
+            else if (name == "Number")
+                formattedPattern = FormatPattern(!lower, !upper, number, !white, !multiple);
+            else if (name.StartsWith("Number") && name.EndsWith(AndWhitespaces))
+                formattedPattern = FormatPattern(!lower, !upper, number, white, !multiple);
+
+            pattern.Add(formattedPattern);
             return pattern;
         }
     }
