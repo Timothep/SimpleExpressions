6388a0f84b220bfb5ec1dcfd6d9c8a1a7774a0d3
Tim Bourguignon <timothepdotnet@gmail.com>
2014-01-21 16:21:07 +0100

    Structural check example changes

--- SimpleExpression/SimpleExpression.Test/StructuralChecksTests.cs
+++ SimpleExpression/SimpleExpression.Test/StructuralChecksTests.cs
@@ -30,39 +30,40 @@ namespace SimpleExpressions.Test
         }
 
         [TestMethod]
-        [Ignore]
         public void RepeatAtLeastAtMostTimesStructuralChecksTest()
         {
             dynamic se = new SimpleExpression();
 
             SimpleExpression result = se
                 .StartsWith
-                .Group
-                    .Text("Repeat").Anything
-                    .Either
-                        .Group
-                            .Text("AtLeast").Anything
-                            .Maybe("AtMost").Anything
-                        .Together
-                    .Or
-                        .Group
-                            .Text("Exactly").Anything
-                        .Together
-                     .Then
-                    .Text("Times")
-                .Together
-                .EndOfLine
+                .Either
+                    .Group
+                        .Text("AtLeast").One("\\(").Number.AtLeast(1).One("\\)")
+                        .Maybe("\\.AtMost").One("\\(").Number.AtLeast(1).One("\\)")
+                    .Together
+                .Or
+                    .Group
+                        .Text("Exactly").One("\\(").Number.AtLeast(1).One("\\)")
+                    .Together
+                    .Then
+                .Text("\\.Times")
                 .Generate();
 
-            var regex = new Regex(result.Expression, RegexOptions.ExplicitCapture);
-            Assert.IsTrue(regex.IsMatch("Repeat.http.AtLeast(2).Times"), "AtLeast");
-            Assert.IsTrue(regex.IsMatch("Repeat.http.AtLeast(2).AtMost(5).Times"), "AtLeast.AtMost");
-            Assert.IsTrue(regex.IsMatch("Repeat.http.Exactly(2).Times"), "Exactly");
+
+            const string desiredPattern = @"^((AtLeast\([0-9]{1,}\)(\.AtMost\([0-9]{1,}\))?)|(Exactly\([0-9]{1,}\))).Times";
             
-            //Wrong, the produced regex has too many wildcards that make the matching succeed in those cases
-            Assert.IsFalse(regex.IsMatch("Repeat.http.AtMost(2).AtLeast(2).Times"), "AtMost.AtLeast");
-            Assert.IsFalse(regex.IsMatch("Repeat.http.Exactly(2).AtLeast(5).Times"), "Exactly.AtLeast");
-            Assert.IsFalse(regex.IsMatch("Repeat.http.Exactly(2).AtMost(5).Times"), "Exactly.AtMost");
+            Assert.AreEqual(desiredPattern, result.Expression);
+            var regex = new Regex(result.Expression, RegexOptions.ExplicitCapture);
+
+            //Positive tests
+            Assert.IsTrue(regex.IsMatch("AtLeast(2).Times"), "AtLeast");
+            Assert.IsTrue(regex.IsMatch("AtLeast(2).AtMost(5).Times"), "AtLeast.AtMost");
+            Assert.IsTrue(regex.IsMatch("Exactly(2).Times"), "Exactly");
+            
+            //Negative tests
+            Assert.IsFalse(regex.IsMatch("AtMost(2).AtLeast(2).Times"), "AtMost.AtLeast");
+            Assert.IsFalse(regex.IsMatch("Exactly(2).AtLeast(5).Times"), "Exactly.AtLeast");
+            Assert.IsFalse(regex.IsMatch("Exactly(2).AtMost(5).Times"), "Exactly.AtMost");
         }
 
         [TestMethod]
