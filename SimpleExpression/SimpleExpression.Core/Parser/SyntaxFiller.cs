ecb1890757081f51ab5b391b0f94286fb25923df
Tim Bourguignon <timothepdotnet@gmail.com>
2014-02-04 14:11:43 +0100

    Layout

--- SimpleExpression/SimpleExpression.Core/Parser/SyntaxFiller.cs
+++ SimpleExpression/SimpleExpression.Core/Parser/SyntaxFiller.cs
@@ -3,23 +3,28 @@ using SimpleExpressions.Core.Converters;
 
 namespace SimpleExpressions.Core.Parser
 {
-    public class SyntaxFiller
+    public static class SyntaxFiller
     {
-        public IList<IConverter> AddMissingOperators(IList<IConverter> converterChain)
+        /// <summary>
+        ///   Crawls through a converter chain and adds the omited "FollowedBy" operators
+        /// </summary>
+        public static IList<IConverter> AddMissingOperators(IList<IConverter> converterChain)
         {
             var newChain = new List<IConverter>(0);
             for (var index = 0; index < converterChain.Count; index ++)
             {
                 newChain.Add(converterChain[index]);
 
-                if (index == converterChain.Count - 1)
-                    break;
+                if (index == converterChain.Count - 1) break;
 
-                if ((converterChain[index].NodeType != NodeType.Operator && converterChain[index].NodeType != NodeType.PrefixedQualifier && converterChain[index + 1].NodeType == NodeType.SimpleNode) ||
-                    (converterChain[index].NodeType == NodeType.SimpleNode && (converterChain[index + 1].NodeType == NodeType.Operator || converterChain[index + 1].NodeType == NodeType.PrefixedQualifier)))
+                if ((converterChain[index].NodeType != NodeType.Operator
+                     && converterChain[index].NodeType != NodeType.PrefixedQualifier
+                     && converterChain[index + 1].NodeType == NodeType.SimpleNode)
+                    || (converterChain[index].NodeType == NodeType.SimpleNode
+                        && (converterChain[index + 1].NodeType == NodeType.Operator
+                            || converterChain[index + 1].NodeType == NodeType.PrefixedQualifier)))
                     newChain.Add(new FollowedBy());
             }
-
             return newChain;
         }
     }
