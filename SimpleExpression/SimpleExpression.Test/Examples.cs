8c49cbb0d4461c7dd0a794056c899c126ed20bfa
Tim Bourguignon <timothepdotnet@gmail.com>
2014-02-07 12:38:31 +0100

    Added an example test file to gather real-life examples

---
+++ SimpleExpression/SimpleExpression.Test/Examples.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleExpressions.Core;
+
+namespace SimpleExpressions.Test
+{
+    [TestClass]
+    public class Examples
+    {
+        [Ignore]
+        [TestMethod]
+        public void HttpFtpLinkMatch()
+        {
+            dynamic se = new SimpleExpression();
+
+            var result = se
+                .Group.Text("http").Then.Maybe("s").Together
+                .Or.Text("ftp")
+                .Then.Text("://").Then.Maybe("www.").Then.OneOrMore.AnythingBut(" ")
+                .Generate();
+
+            //Match the result...
+        }
+    }
+}
