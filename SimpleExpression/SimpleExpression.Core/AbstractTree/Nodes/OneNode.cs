3eab2805606322b3e421222edf2a125e1580b023
Tim Bourguignon <timothepdotnet@gmail.com>
2014-03-08 23:17:28 +0100

    Escapint the "." in a One Function

--- SimpleExpression/SimpleExpression.Core/AbstractTree/Nodes/OneNode.cs
+++ SimpleExpression/SimpleExpression.Core/AbstractTree/Nodes/OneNode.cs
@@ -5,5 +5,13 @@ namespace SimpleExpressions.Core.AbstractTree.Nodes
     public class OneNode : ValueNode
     {
         public OneNode(IConverter converter) : base(converter) { }
+
+        public override string Generate()
+        {
+            if(this.Value == ".")
+                this.Value = this.Value.Insert(0,@"\");
+
+            return base.Generate();
+        }
     }
 }
